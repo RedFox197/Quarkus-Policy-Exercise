ead2be82d55f93badff9cf3313527434a303d3dc
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-07-10 15:02:13 +0200

    sistemato configuration-service

--- configuration-service/src/main/java/com/github/redfox197/Configuration.java
+++ configuration-service/src/main/java/com/github/redfox197/Configuration.java
@@ -5,12 +5,14 @@ import io.quarkus.mongodb.panache.common.MongoEntity;
 import org.bson.Document;
 import org.bson.codecs.pojo.annotations.BsonExtraElements;
 
-@MongoEntity(database = "configurations", collection = "configuration")
+import java.util.Map;
+
+@MongoEntity(collection = "configuration")
 public class Configuration extends PanacheMongoEntity {
     public String name;
-    public int pippo;
 
-    // FIXME credo di aver capito che funziona solo in lettura da Mongodb
+    public Map<String, Object> fields;
+
     @BsonExtraElements
     public Document extra;
 }
--- configuration-service/src/main/java/com/github/redfox197/ConfigurationResource.java
+++ configuration-service/src/main/java/com/github/redfox197/ConfigurationResource.java
@@ -12,6 +12,12 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 public class ConfigurationResource {
 
+    @POST
+    public Response create(Configuration configuration) {
+        configuration.persist();
+        return Response.created(URI.create("/api/v1/configurations/" + configuration.id)).build();
+    }
+
     @GET
     public List<Configuration> findAll() {
         return Configuration.listAll();
@@ -23,30 +29,23 @@ public class ConfigurationResource {
         return Configuration.findById(new ObjectId(id));
     }
 
-    @POST
-    public Response create(Configuration configuration) {
-        configuration.persist();
+    @PUT
+    @Path("/{id}")
+    public Configuration update(@PathParam("id") String id, Configuration configuration) {
+        ObjectId objectId = new ObjectId(id);
 
-        configuration.extra.entrySet().stream()
-                .map(entry -> entry.getKey() + entry.getValue())
-                .forEach(System.out::println);
+        if (Configuration.findById(objectId) == null) {
+            throw new NotFoundException();
+        }
 
-        return Response.created(URI.create("/api/v1/configurations/" + configuration.id)).build();
+        configuration.id = objectId;
+        configuration.update();
+        return configuration;
     }
 
     @DELETE
     @Path("/{id}")
     public void delete(@PathParam("id") String id) {
-        Configuration configuration = Configuration.findById(new ObjectId(id));
-        if (configuration == null) throw new NotFoundException();
-        configuration.delete();
-    }
-
-    @PUT
-    @Path("/{id}")
-    public Configuration update(@PathParam("id") String id, Configuration configuration) {
-        //configuration.id = new ObjectId(id);
-        configuration.update();
-        return configuration;
+        Configuration.deleteById(new ObjectId(id));
     }
 }
